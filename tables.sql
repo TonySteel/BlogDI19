327576e7a6c40075b50cc5f9c17d742ae493d1b4
Tony Steel <60441474+TonySteel@users.noreply.github.com>
2020-11-14 11:27:05 +0100

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,11 @@
+créer  des  articles de table
+(
+    Id               bigint auto_increment
+        clé primaire ,
+    Titre            varchar ( 50 )   nul ,
+Texte de              description      nul ,
+    DateAjout        date          null ,
+    Auteur           varchar ( 50 )   nul ,
+    ImageRepository varchar ( 50 )   null ,
+    ImageFileName    varchar ( 255 ) null
+);
